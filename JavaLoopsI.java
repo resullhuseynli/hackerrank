bb4e6f62c2664e0f1c532f49958839fef4d711bc
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-16 16:24:54 +0400

    Java Loops I added!

---
+++ JavaLoopsI.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoopsI {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        for (int i=1 ; i<=10 ; i++) {
+
+            System.out.println(N + " x " + i + " = " + i*N);
+
+        }
+
+    }
+
+}
